5a1bfec8b3237f74fb04a323d60a2abf6aecf665
YuvrajxGarg <workwithyuvraj@gmail.com>
2023-06-01 00:37:03 +0530

    now insult multiple people

--- commands/info/insult.js
+++ commands/info/insult.js
@@ -3,18 +3,35 @@ const insulter = require('insult');
 
 module.exports = {
 	name: 'insult',
-	description: "insults the user or anyone who is mentio",
+	description: "insults the user or anyone who is mentioned",
 	userPerms: [],
 	botPerms: [],
 	run: async (client, message, args) => {
-        //console.log(args);
-        const member = message.mentions.members.first()
-        if (!member) { 
-            message.channel.send("You have to mention a person to insult")
+        const member = message.mentions.members
+        if (!member.first()) { 
+        const embed = new EmbedBuilder()
+		
+		.setDescription(`You have to mention a person to insult`)
+		.setColor('#03fcdb')
+		
+	
+	         message.reply({ embeds: [embed]})
+  
         }
         else{
-    		message.channel.send("<@"+member+"> " + insulter.Insult()) 
-        }
+            
+            const arr = member.first(member.size)
+           // 
+            for(b in arr)
+            {
+            const embed = new EmbedBuilder()
+            .setDescription(insulter.Insult() )
+           // console.log(arr[b])
+           
+          message.channel.send({ content : "<@"+arr[b].user.id+"> " , embeds : [embed]})
+            
+            }
         
 	}
+}
 }
